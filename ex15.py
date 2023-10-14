18987acb0d2e56c0bd496dadb65aaccf8f768fb3
antant2786 <anthonyly2786@hotmail.com>
2023-10-14 10:48:41 -0400

    added comments to ex15

--- ex15.py
+++ ex15.py
@@ -1,9 +1,13 @@
+# this imports the argv module from sys
 from sys import argv
 
+# this is setting the name of variables you need to include when running the script
 script, filename = argv
 
+# this is setting a variale for "txt" and using it to open "filename"
 txt = open(filename)
 
+# 
 print(f"Here's your file {filename}:")
 print(txt.read())
 
